fc39d96cc9c50e366d6c5e915ce62fcff1bdab65
Julian Conneely <julian.conneely@hotmail.com>
2018-10-05 19:12:14 +0100

    commit from home

--- October3.py
+++ October3.py
@@ -2,4 +2,4 @@
 # testing push and pull
 # again
 # and again
-# extra comment test, failed on LAN, should commit on wifi at work?
+# extra comment test, failed on LAN, should commit on wifi at work? no it didnt, at home?
