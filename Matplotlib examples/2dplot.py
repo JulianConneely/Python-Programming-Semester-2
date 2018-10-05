a2171c92c191f2a37dbe20830349927853d94844
JulianConneely <36239852+JulianConneely@users.noreply.github.com>
2018-10-05 20:41:49 +0100

    new 2d pyplot

---
+++ Matplotlib examples/2dplot.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'b.') # plots x (horizontal) and y (vertical) axis. b. uses a bold dot instead of the default line in the plot
+plt.ylabel("Some numbers")
+plt.show()
